825ace4ef6dbc81a91a57a6954ae8fa07a27ad47
280051hubert <hubertlozinski7@gmail.com>
2026-03-18 17:22:02 +0100

    zadanie 4.0

---
+++ laboratorium1_test/Test1.cs
@@ -0,0 +1,15 @@
+﻿using ;
+using System.Runtime.CompilerServices;
+[assembly: InternalsVisibleTo("laboratorium1_test")]
+
+namespace laboratorium1_test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
